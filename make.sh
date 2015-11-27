f572e4025ef6124851f8d40b868fe4d991ab8eb8
Jake Correa <jcorrea257@gmail.com>
2015-11-27 15:25:28 -0500

    Remove oh-my-zsh

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="dbignore dropbox.py gitconfig gitignore_global vimrc vim zshrc oh-my-zsh zsh_aliases irbrc"    # list of files/folders to symlink in homedir
+files="dbignore dropbox.py gitconfig gitignore_global vimrc vim zshrc zsh_aliases irbrc"    # list of files/folders to symlink in homedir
 
 ##########
 
