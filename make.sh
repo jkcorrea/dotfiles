97156f3d61565f142d28589ba5106f2ae036d051
Jake Correa <jacob_correa@apple.com>
2018-02-27 17:11:52 +0900

    Add iterm configs and fix make for one last time

--- make.sh
+++ make.sh
@@ -29,43 +29,54 @@ cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    [ -s ~/.$file ] && mv ~/.$file $olddir/
+    if [ -s ~/.$file ]; then
+        echo "Moving existing file $file from ~ to $olddir"
+        rm -rf $olddir/.$file
+        mv ~/.$file $olddir/
+        rm -rf ~/.$file
+    fi
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
 
 install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
+    # Try to install zsh if it isn't
+    if [ ! $(grep /zsh$ /etc/shells | wc -l) -ge 1 ]; then 
+        # Get machine platform
+        platform=$(uname);
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            if [[ -f /etc/redhat-release ]]; then
+                sudo yum install zsh
+                install_zsh
+            fi
+            if [[ -f /etc/debian_version ]]; then
+                sudo apt-get install zsh
+                install_zsh
+            fi
+        # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            if [ -s /usr/local/bin/brew ]; then
+                brew install zsh
+                install_zsh
+            else
+                echo "Please install Homebrew or zsh and re-run this script."
+                exit
+            fi
+        fi
     fi
+
+    # Install oh-my-zsh
+    if [[ ! -d $dir/.oh-my-zsh/ ]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    fi
+
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
         chsh -s $(which zsh)
     fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
-        fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
-        fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
+
 }
 
 install_zsh
